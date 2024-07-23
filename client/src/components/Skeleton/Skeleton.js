cf85090d6cebfb5a39b3b9c90e2e1c6f519afbad
YurasKamaz <tyertyn@gmail.com>
2024-07-23 10:10:03 +1000

    Add skeleton loading

---
+++ client/src/components/Skeleton/Skeleton.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './styles.module.css'
+
+
+const Skeleton = ({count = 3}) => {
+    return(
+        <>
+            <ul className={styles.list}>
+                {[Array(count)].map((_, index) => (
+                    <li key={index} className={styles.item}></li>
+                ))}
+            </ul>
+        </>
+    )
+}
+
+export default Skeleton
--- client/src/pages/Debtors.js
+++ client/src/pages/Debtors.js
@@ -1,19 +1,18 @@
-import React, { useCallback, useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Button, Card, Col, Container, ListGroup } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
 import { Context } from '..';
-import { DEBTS_ROUTE } from '../utils/consts';
 import { observer } from 'mobx-react-lite';
 import { fetchDebtors } from '../http/debtorsAPI';
 import DebtorModal from '../components/modals/DebtorModal';
+import Skeleton from '../components/Skeleton/Skeleton';
 
 
 const Debtors = observer(() => {
     const {debtors} = useContext(Context)
     const {user} = useContext(Context)
-    const navigate = useNavigate()
     const [modalVisible, setModalVisible] = useState('')
     const [modalDebtorId, setModalDebtorId] = useState(0)
+    const [isLoading,  setIsLoading] = useState(true)
     
     const remDebt = async (id) => {
         try {
@@ -25,32 +24,40 @@ const Debtors = observer(() => {
     }
 
     useEffect(() => {
-        fetchDebtors(user.user.id).then(data => debtors.setDebtors(data))   
+        setIsLoading(true)
+        fetchDebtors(user.user.id).then(data => debtors.setDebtors(data))
+        setIsLoading(false)
     }, [debtors.debtors.length])
 
     return(
-        <Container className='d-flex justify-content-between mt-3 ' style={{width: window.innerHeight - 100}}>
-            <Col>
-                <ListGroup>
-                    {!!debtors.debtors.length && 
-                        <Card>
-                            {debtors.debtors.map(debtor =>
-                                <ListGroup.Item key = {debtor.id} style={{cursor: 'pointer'}} onClick={() => {setModalVisible(true); setModalDebtorId(debtor.id)}}>
-                                    <Container className='d-flex justify-content-between'>
-                                        {debtor.name}
-                                        <Button variant='danger' onClick={(event) => {
-                                            event.stopPropagation();
-                                            remDebt(debtor.id)
-                                        }}>✖</Button>
-                                    </Container>                 
-                                </ListGroup.Item>
-                            )}                         
-                        </Card>
-                    } 
-                    <DebtorModal show={modalVisible} onHide={() => setModalVisible(false)} debtorId={modalDebtorId}/>
-                </ListGroup>               
-            </Col>           
-        </Container>        
+        <>
+            {!isLoading ?
+                <Container className='d-flex justify-content-between mt-3 ' style={{width: window.innerHeight - 100}}>
+                    <Col>
+                        <ListGroup>
+                            {!!debtors.debtors.length && 
+                                <Card>
+                                    {debtors.debtors.map(debtor =>
+                                        <ListGroup.Item key = {debtor.id} style={{cursor: 'pointer'}} onClick={() => {setModalVisible(true); setModalDebtorId(debtor.id)}}>
+                                            <Container className='d-flex justify-content-between'>
+                                                {debtor.name}
+                                                <Button variant='danger' onClick={(event) => {
+                                                    event.stopPropagation();
+                                                    remDebt(debtor.id)
+                                                }}>✖</Button>
+                                            </Container>                 
+                                        </ListGroup.Item>
+                                    )}                         
+                                </Card>
+                            } 
+                            <DebtorModal show={modalVisible} onHide={() => setModalVisible(false)} debtorId={modalDebtorId}/>
+                        </ListGroup>               
+                    </Col>           
+                </Container>  
+                :
+                <Skeleton/>
+            }
+        </>      
     )
 })
 
